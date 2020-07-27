96ca75f44fe93852095f6f073f7b9d60342eed2e
CJ Fulton <fulchr3356@gmail.com>
2020-07-27 10:38:55 -0400

    Completed ArcadeAccountManager

--- src/main/java/com/github/curriculeon/Arcade.java
+++ src/main/java/com/github/curriculeon/Arcade.java
@@ -22,7 +22,7 @@ public class Arcade implements Runnable {
         String arcadeDashBoardInput;
         do {
             arcadeDashBoardInput = getArcadeDashboardInput();
-            ArcadeAccountManager arcadeAccountManager = new ArcadeAccountManager();
+            ArcadeAccountManager arcadeAccountManager =  ArcadeAccountManager.getInstance();
             if ("select-game".equals(arcadeDashBoardInput)) {
                 String accountName = console.getStringInput("Enter your account name:");
                 String accountPassword = console.getStringInput("Enter your account password:");
@@ -42,7 +42,7 @@ public class Arcade implements Runnable {
                 } else {
                     // TODO - implement better exception handling
                     String errorMessage = "No account found with name of [ %s ] and password of [ %s ]";
-                    throw new RuntimeException(String.format(errorMessage, accountPassword, accountName));
+                    throw new RuntimeException(String.format(errorMessage, accountName, accountPassword));
                 }
             } else if ("create-account".equals(arcadeDashBoardInput)) {
                 console.println("Welcome to the account-creation screen.");
--- src/main/java/com/github/curriculeon/arcade/ArcadeAccountManager.java
+++ src/main/java/com/github/curriculeon/arcade/ArcadeAccountManager.java
@@ -1,17 +1,33 @@
 package com.github.curriculeon.arcade;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by leon on 7/21/2020.
  * `ArcadeAccountManager` stores, manages, and retrieves `ArcadeAccount` objects
  * it is advised that every instruction in this class is logged
  */
 public class ArcadeAccountManager {
+    private final static ArcadeAccountManager INSTANCE = new ArcadeAccountManager();
+    private final Map<String,ArcadeAccount> userKeys = new HashMap<>();
+
+    public static ArcadeAccountManager getInstance(){
+        return INSTANCE;
+    }
+
+    private ArcadeAccountManager(){}
     /**
      * @param accountName name of account to be returned
      * @param accountPassword password of account to be returned
      * @return `ArcadeAccount` with specified `accountName` and `accountPassword`
      */
     public ArcadeAccount getAccount(String accountName, String accountPassword) {
+        if (userKeys.containsKey(accountName)) {
+            if (userKeys.get(accountName).getAccountPassword().equals(accountPassword))
+                return userKeys.get(accountName);
+        }
+        System.out.println(userKeys);
         return null;
     }
 
@@ -21,8 +37,9 @@ public class ArcadeAccountManager {
      * @param accountPassword password of account to be created
      * @return new instance of `ArcadeAccount` with specified `accountName` and `accountPassword`
      */
-    public ArcadeAccount createAccount(String accountName, String accountPassword) {
-        return null;
+    public ArcadeAccount createAccount(String accountName, String accountPassword)
+    {
+        return new  ArcadeAccount(accountName,accountPassword);
     }
 
     /**
@@ -30,5 +47,9 @@ public class ArcadeAccountManager {
      * @param arcadeAccount the arcadeAccount to be added to `this.getArcadeAccountList()`
      */
     public void registerAccount(ArcadeAccount arcadeAccount) {
+        userKeys.put(arcadeAccount.getAccountName(),arcadeAccount);
+
     }
+
+
 }
