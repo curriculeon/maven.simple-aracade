8191bf39f83c68ea0e5b6c4f054a138f462ce66b
Alonso Ninalaya <pikachualonso@gmail.com>
2020-08-03 22:27:55 -0400

    changes being made

--- src/main/java/com/github/curriculeon/arcade/ArcadeAccountManager.java
+++ src/main/java/com/github/curriculeon/arcade/ArcadeAccountManager.java
@@ -1,17 +1,31 @@
 package com.github.curriculeon.arcade;
 
+import com.github.curriculeon.utils.AnsiColor;
+import com.github.curriculeon.utils.IOConsole;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by leon on 7/21/2020.
  * `ArcadeAccountManager` stores, manages, and retrieves `ArcadeAccount` objects
  * it is advised that every instruction in this class is logged
  */
 public class ArcadeAccountManager {
+
+    private IOConsole console = new IOConsole(AnsiColor.YELLOW);
+    private List<ArcadeAccount> arcadeAccountList = new ArrayList<>();
     /**
      * @param accountName name of account to be returned
      * @param accountPassword password of account to be returned
      * @return `ArcadeAccount` with specified `accountName` and `accountPassword`
      */
     public ArcadeAccount getAccount(String accountName, String accountPassword) {
+        for(ArcadeAccount account : arcadeAccountList) {
+            if(accountName.equals(account.getUsername()) && accountPassword.equals(account.getPassword())) {
+                return account;
+            }
+        }
         return null;
     }
 
@@ -22,7 +36,10 @@ public class ArcadeAccountManager {
      * @return new instance of `ArcadeAccount` with specified `accountName` and `accountPassword`
      */
     public ArcadeAccount createAccount(String accountName, String accountPassword) {
-        return null;
+        console.println("Attempting to create account with name [ %s ] and password [ %s ]", accountName, accountPassword);
+        ArcadeAccount arcadeAccount = new ArcadeAccount(accountName, accountPassword);
+        console.println("Successfully created account with name [ %s ] and password [ %s ]", accountName, accountPassword);
+        return arcadeAccount;
     }
 
     /**
@@ -30,5 +47,6 @@ public class ArcadeAccountManager {
      * @param arcadeAccount the arcadeAccount to be added to `this.getArcadeAccountList()`
      */
     public void registerAccount(ArcadeAccount arcadeAccount) {
+        arcadeAccountList.add(arcadeAccount);
     }
 }
--- src/main/java/com/github/curriculeon/arcade/slots/SlotsGame.java
+++ src/main/java/com/github/curriculeon/arcade/slots/SlotsGame.java
@@ -1,7 +1,42 @@
 package com.github.curriculeon.arcade.slots;
 
+import com.github.curriculeon.arcade.ArcadeAccount;
+import com.github.curriculeon.arcade.GameInterface;
+import com.github.curriculeon.arcade.PlayerInterface;
+import com.github.curriculeon.utils.AnsiColor;
+import com.github.curriculeon.utils.IOConsole;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by leon on 7/21/2020.
  */
-public class SlotsGame {
+public class SlotsGame implements GameInterface {
+
+    private IOConsole console = new IOConsole(AnsiColor.GREEN);
+    private List<PlayerInterface> playerList = new ArrayList<>();
+    private SlotsReel slotsReel = new SlotsReel();
+
+    @Override
+    public void add(PlayerInterface player) {
+        playerList.add(player);
+    }
+
+    @Override
+    public void remove(PlayerInterface player) {
+        playerList.remove(player);
+    }
+
+    @Override
+    public void run() {
+        for (PlayerInterface playerInterface : playerList) {
+            String userInput;
+            do {
+                SlotsPlayer slotsPlayer = (SlotsPlayer) playerInterface;
+                userInput = console.getStringInput("pull-lever, view-slot-reel, quit");
+                slotsPlayer.play(slotsReel, userInput);
+            } while(!"quit".equals(userInput));
+        }
+    }
 }
